0f95c8e54694efd3bedd3b45aa777bbd962464e8
Colin Cason <48043196+colinjcason@users.noreply.github.com>
2024-02-06 20:47:38 -0800

    created sign in component and set up firebase utils

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -1,11 +1,11 @@
-import { signInWithGooglePopup } from '../../utils/firebase'
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase'
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
+    const {user} = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
   }
-  
+
   return (
     <div>
       <h2>SignIn</h2>
